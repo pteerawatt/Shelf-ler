96dc288e1ce9cc051789a00851dbbaef68f5649b
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2021-02-24 15:37:13 -0800

    render image on sidebar

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
+import SidebarItem from './SidebarItems';
+import SidebarImage from './SidebarItems';
 
 const Sidebar: React.FC = () => {
   return (
     <div className="sidebar">
       <ul>
-        <div>
-          sidebar
-        </div>
-        <div>
-          sidebar
-        </div>
-        <div>
-          sidebar
-        </div>
-        <div>
-          sidebar
-        </div>
+        <li>
+          <SidebarItem />
+        </li>
+        <li>
+          <SidebarItem />
+        </li>
+        <li>
+          <SidebarItem />
+        </li>
+        <li>
+          <SidebarItem />
+        </li>
       </ul>
     </div>
   )
