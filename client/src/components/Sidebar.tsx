5dcf60dcd3d5f97099101e361c18caca84b2675e
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2021-02-24 12:55:27 -0800

    rendered main components onto app

---
+++ client/src/components/Sidebar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Sidebar: React.FC = () => {
+  return (
+    <div>
+      <ul>
+        <div>
+          sidebar
+        </div>
+        <div>
+          sidebar
+        </div>
+        <div>
+          sidebar
+        </div>
+        <div>
+          sidebar
+        </div>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar;
--- client/src/components/app.tsx
+++ client/src/components/app.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
-import Header from './Header'
+import Sidebar from './Sidebar';
+import Toolbar from './Toolbar';
+import Display from './Display';
 
 const App: React.FC = () => {
   return (
     <div>
-      <Header />
+      <header>
+        <a href="/">Shelf-ly</a>
+      </header>
+      <main>
+        <div className="main-container">
+          <Sidebar />
+          <div className="toolbar-display-container">
+            <Toolbar />
+            <Display />
+          </div>
+        </div>
+      </main>
+      <footer>
+        All rights is reserved.
+      </footer>
     </div>
   )
 }
